765d85b29b8cbe55b0788b8ae72e5303b8bef6f9
ini-tanmay <54876707+ini-tanmay@users.noreply.github.com>
2020-09-02 18:44:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def hello():
 
 if __name__ == '__main__':
     from api import *
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(threaded=True, port=5000)
+
