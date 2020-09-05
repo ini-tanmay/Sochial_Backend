1c1678428d0daf874e2d4f81ccec7a636ba8ae5d
ini-tanmay <54876707+ini-tanmay@users.noreply.github.com>
2020-09-05 17:14:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,19 +30,52 @@ app.config.from_object(MyConfig)
 mongo = PyMongo(app)
 from api import *
 
-#
-# {
-#     "_id": "tanmay".
-#     "username": "tanmay",
-#
-#     "name": "name"
-# }
+@app.route('/api/v1.0/users/id/<string:userID>/followers/<int:last_no>', endpoint='get_followers_list')
+def get_followers_list(userID, last_no):
+    output = []
+    followers = mongo.db.followers
+    users = followers.find({'_id': ObjectId(userID)}, {'followersList': {'$slice': [last_no * 30, (last_no + 1) * 30]}})
+    for i in users:
+        output.append(i['followersList'])
+    return jsonify(output[0])
 
-# userID = request.args['username']
-# bio = request.args['bio']
-# fcm
-# userID = request.args['name']
-# websiteLink = request.args['websiteLink']
+
+@app.route('/api/v1.0/users/id/<string:userID>/following',endpoint='get_following_list')
+def get_following_list(userID):
+    output = []
+    followers = mongo.db.followers
+    users = followers.find({}, {
+        'followersList': {'$elemMatch': {'_id': ObjectId(userID)}}
+        , '_id': 1, 'name': 1, 'usern': 1})
+    for i in users:
+        output.append(i)
+    return jsonify(output)
+
+
+@app.route('/api/v1.0/users/id/<string:otherUserID>/name/<string:name>/username/<string:username>/follow',endpoint='follow_other_user')
+           methods=['POST'])
+def myuser_follows_otheruser(otherUserID, name, username):
+    followers = mongo.db.followers
+    myUserDict = request.get_json(force=True)
+    myUserDict['_id'] = ObjectId(myUserDict['_id'])
+    followers.update_one({'_id': ObjectId(otherUserID)},
+                         {'$push': {'followersList': myUserDict}, '$set': {'name': name, 'usern': username}},
+                         upsert=True)
+    return jsonify(True)
+
+
+@app.route('/api/v1.0/users/id/<string:myUserID>/unfollow/<string:otherUserID>',endpoint='unfollow_other_user')
+def myuser_unfollows_otheruser(myUserID, otherUserID):
+    followers = mongo.db.followers
+    followers.update(
+        {"_id": ObjectId(otherUserID)},
+        {
+            "$pull": {
+                "followersList": {"_id": ObjectId(myUserID)}
+            }
+        })
+    return jsonify(True)
+
 
 @app.route('/api/v1.0/users/username/<string:username>')
 def is_user_taken(username):
