d0be9e7c5ee56fcf453026f5ea3bb52ffb8bc031
ini-tanmay <54876707+ini-tanmay@users.noreply.github.com>
2020-09-19 16:10:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ ScoutApm(app)
 app.config["SCOUT_NAME"] = "Sochial"
 
 
-# from api import *
+from api import *
 # restServerInstance.add_resource(User, "/api/v1.0/users/id/<string:userID>/followers")
 # restServerInstance.add_resource(User, "/api/v1.0/users/id/<string:userID>/followers")
 # restServerInstance.add_resource(User, "/api/v1.0/users/id/<string:userID>/following/")
@@ -283,6 +283,4 @@ def hello():
 
 if __name__ == '__main__':
     app.logger.debug("Starting Flask Server")
-    from api import *
-
-    app.run(host='192.168.1.69', port=5065, debug=False, use_reloader=True)
+    app.run(threaded=True)
