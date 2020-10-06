95ae875fd35895b3922616af68e45ebdc91dd05c
cmd-dev <tanmay.armal@somaiya.edu>
2020-10-06 15:11:07 +0530

    added newest and top filter

--- api/post/postapi.py
+++ api/post/postapi.py
@@ -17,6 +17,7 @@ def get_db_reference(type):
 class Post(AppResource):
 
     def __init__(self):
+        self.users=mongo.db.users
         pass
 
     def post(self, type):
@@ -25,27 +26,52 @@ class Post(AppResource):
         postDict = request.get_json(force=True)
         doc_id = get_db_reference(type).insert_one(postDict).inserted_id
         return {'id': doc_id}, 200
-        
+
     def get(self, type):
         # if last id is null return first few items upto the limit
         # if last id is not null return all items from the next item of last id upto the limit
         output = []
         firstPosts = None
         limit = int(request.args['limit'])
-        try:
-            lastDocumentID = request.args['last_id']
-        except:
-            firstPosts = get_db_reference(type).find(sort=[('_id', pymongo.ASCENDING)]).limit(limit)
-            for i in firstPosts:
+        filter = request.args['filter']
+        if filter=='newest':
+            try:
+                lastDocumentID = request.args['last_id']
+            except:
+                firstPosts = get_db_reference(type).find(sort=[('_id', pymongo.DESCENDING)]).limit(limit)
+                for i in firstPosts:
+                    i['timeStamp'] = int(ObjectId(i['_id']).generation_time.timestamp() * 1000)
+                    output.append(i)
+                return output,200
+            posts = get_db_reference(type).find({'_id': {'$gt': ObjectId((lastDocumentID))}}).sort('_id',
+                                                                                                   pymongo.DESCENDING).limit(limit)
+            for i in posts:
                 i['timeStamp'] = int(ObjectId(i['_id']).generation_time.timestamp() * 1000)
                 output.append(i)
-            return output,200
-        posts = get_db_reference(type).find({'_id': {'$gt': ObjectId((lastDocumentID))}}).sort('_id',
-                                                                                               pymongo.ASCENDING).limit(limit)
-        for i in posts:
-            i['timeStamp'] = int(ObjectId(i['_id']).generation_time.timestamp() * 1000)
-            output.append(i)
-        return output, 200
+            return output, 200
+        elif filter=='top':
+            try:
+                lastDocumentID = request.args['last_id']
+            except:
+                firstPosts = get_db_reference(type).find(sort=[('likes', pymongo.DESCENDING)]).limit(limit)
+                for i in firstPosts:
+                    i['timeStamp'] = int(ObjectId(i['_id']).generation_time.timestamp() * 1000)
+                    output.append(i)
+                return output,200
+            posts = get_db_reference(type).find({'_id': {'$gt': ObjectId((lastDocumentID))}}).sort('likes',
+                                                                                                   pymongo.DESCENDING).limit(limit)
+            for i in posts:
+                i['timeStamp'] = int(ObjectId(i['_id']).generation_time.timestamp() * 1000)
+                output.append(i)
+            return output, 200
+
+
+
 
     def put(self, type):
         pass
+
+    def delete(self, type):
+        pass
+        # self.users.update_one({'_id':userID},{'$inc':{'posts':-1}})
+
